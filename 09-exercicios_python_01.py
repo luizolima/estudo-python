b46d3b11373f232ed6a731cc68c09cbfc413d674
Luiz Otávio Lima <luiz.oflima@outlook.com>
2021-04-25 23:33:21 -0300

    Realização de alguns exercícios em Python

---
+++ 09-exercicios_python_01.py
@@ -0,0 +1,46 @@
+# Exercicio 01
+"""
+numero = int(input('Digite um número inteiro: '))
+print(numero)
+"""
+# Exercicio 02
+"""
+num_real = float(input('Digite um número real: '))
+print(num_real)
+"""
+# Exercicio 03
+"""
+a = int(input('Digite o primeiro valor inteiro: '))
+b = int(input('Digite o segundo valor inteiro: '))
+c = int(input('Digite o terceiro valor inteiro: '))
+soma = a + b + c
+print(f'A soma é: {soma}')
+"""
+# Exercicio 04
+"""
+num_real = float(input('Digite um número real: '))
+exp = num_real ** 2
+print(f'O quandrado de {num_real} é: {exp}')
+"""
+# Exercicio 05
+"""
+num_real = float(input('Digite um número real: '))
+q_part = num_real / 5
+print(f'A quinta parte de {num_real} é: {q_part}')
+"""
+# Exercicio 06
+"""
+c = float(input('Digite a temp. em graus celsius: '))
+f = (c * (9/5)) + 32
+print(f'A temperatura em graus celsius {c} em fahrenheit é: {f}')
+"""
+# Exercicio 28
+"""
+a = float(input('Primeiro número: '))
+b = float(input('Segundo número: '))
+c = float(input('Terceiro número: '))
+
+op = a ** 2 + b ** 2 + c **2
+print(op)
+"""
+
