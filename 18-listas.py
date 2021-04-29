ce183aeacead4a34163bc3dfc440590481cab3e3
Luiz Otávio Lima <luiz.oflima@outlook.com>
2021-04-28 22:00:16 -0300

    Adição da aula sobre Listas em Python

---
+++ 18-listas.py
@@ -0,0 +1,38 @@
+"""
+Listas
+
+Listas em Python funcionam como vetores/matrizes (arrays) em outras Linguagens, com a diferença
+de serem DINÂMICO e também de podermos colocar QUALQUER tipo de dado.
+
+Linguagens C/Java: Arrays
+    - Possuem tamanho e tipo de dado fixo;
+    Ou seja, nestas linguagens se você criar um array do tipo int e com tamanho 5, este array
+    sera SEMPRE do tipo inteiro e poderá ter SEMPRE no máximo 5 valores.
+
+Já em Python:
+
+- Dinâmico: Não possuem tamanho fixo; Ou seja, podemos criar a lista e simplesmente ir adicionando elementos;
+- Qualquer tipo de dado: Não possuem tipo de dado fixo; Ou seja, podemos colocar qualquer tipo de dado;
+
+As listas em Python são representadas por colchetes: []
+"""
+type([])
+
+lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
+lista2 = ['G', 'e', 'e', 'k', ' ', 'U', 'n', 'i', 'v', 'e', 'r', 's', 'i', 't', 'y']
+lista3 = []
+lista4 = list(range(11))
+lista5 = list('Geek University')
+
+# Podemos facilmente checar se determinado valor está contido na lista
+num = 7
+if num in lista4:
+    print(f'Encontrei o número {num}')
+else:
+    print(f'Não encotrei o número {num}')
+
+# Podemos facilmente ordenar uma lista numérica ou alfabética
+lista1.sort()
+print(lista1)
+lista2.sort()
+print(lista2)
