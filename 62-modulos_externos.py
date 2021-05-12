bd910dbe309b51063fdb2e957dba99c455c35707
Luiz Otávio Lima <luiz.oflima@outlook.com>
2021-05-12 18:42:11 -0300

    Aula sobre reduce() em Python

---
+++ 62-modulos_externos.py
@@ -0,0 +1,9 @@
+"""
+Módulos Externos
+
+Utilizamos o gerenciador de pacotes Python chamado Pip - Python Installer Package
+
+Você pode conhecer todos os pacotes externos oficiais em: http://pypi.org
+
+colorama ->  É utilizado para permitier impressão de textos
+"""
