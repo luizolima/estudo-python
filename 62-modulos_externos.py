06e1ec02ac76956cd2e3bc414cf172308f927cdb
Luiz Otávio Lima <luiz.oflima@outlook.com>
2021-05-18 19:01:48 -0300

    Aula sobre Módulos Externos em Python

--- 62-modulos_externos.py
+++ 62-modulos_externos.py
@@ -5,5 +5,17 @@ Utilizamos o gerenciador de pacotes Python chamado Pip - Python Installer Packag
 
 Você pode conhecer todos os pacotes externos oficiais em: http://pypi.org
 
-colorama ->  É utilizado para permitier impressão de textos
+colorama ->  É utilizado para permitier impressão de textos coloridos no terminal.
+
+Instalando um módulo:
+
+pip install nome_do_modulo
+
 """
+# pip install colocarama
+
+# Utilizando o pacote instalado
+from colorama import init, Fore
+init()
+
+print(Fore.GREEN + 'Geek University')
